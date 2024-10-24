c6a9a0715163e24312de95d4937e8a0ff44c4bb6
Samira Esmail Pur <samiraesmailpur05@gmail.com>
2024-10-24 17:12:24 +0300

    removed extra code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes';
 import './index.css';
-import '@radix-ui/themes/styles.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
